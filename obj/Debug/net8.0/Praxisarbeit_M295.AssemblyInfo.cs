62493554fce396359864ea24be0b995950f76c47
Lennyjack <lenny.brun@student.ipso.ch>
2024-12-05 14:33:55 +0100

    Bugfixes + FirstTest Postman: Errors with Login

--- obj/Debug/net8.0/Praxisarbeit_M295.AssemblyInfo.cs
+++ obj/Debug/net8.0/Praxisarbeit_M295.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Praxisarbeit_M295")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa79742b0f00611a88274e9fa67b807930976bd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29476b715e5588346b80a3af46d45f6975a25d82")]
 [assembly: System.Reflection.AssemblyProductAttribute("Praxisarbeit_M295")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Praxisarbeit_M295")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
