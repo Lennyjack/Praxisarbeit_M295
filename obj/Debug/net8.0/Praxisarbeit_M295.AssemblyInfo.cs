29d75baa8088f64749308b58cac85f8d40e9e7c0
Lennyjack <lenny.brun@student.ipso.ch>
2024-12-05 11:20:03 +0100

    ApplicationDBContext

--- obj/Debug/net8.0/Praxisarbeit_M295.AssemblyInfo.cs
+++ obj/Debug/net8.0/Praxisarbeit_M295.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Praxisarbeit_M295")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d71284be8c92eda8a893359edb25f7b06daafb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fabc39d04d749ee8bf163412baa6ba8816da4ba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Praxisarbeit_M295")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Praxisarbeit_M295")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
